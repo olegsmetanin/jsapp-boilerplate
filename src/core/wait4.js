bb4b4a467a17c1ba7540c6672dd833d8fbb8f615
Oleg Smith <olegsmith@gmail.com>
2013-02-19 15:00:39 +0400

    some code beautifying

--- src/core/wait4.js
+++ src/core/wait4.js
@@ -1,23 +1,25 @@
-
-
 function wait4(existsFn, msg) {
-	return function (fn) {
-		var counter=0;
-		function wait() {
-			if (existsFn()) {
-				counter++;
-				if (counter<9) {
-					return window.setTimeout(wait,200*Math.pow(2,counter));
-				} else {
-					throw(msg);
-				}
-			} else {
-				fn();
-			}
-		}
-		wait();
-	}
+    return function (fn) {
+        var counter = 0;
+
+        function wait() {
+            if (existsFn()) {
+                counter++;
+                if (counter < 9) {
+                    window.setTimeout(wait, 200 * Math.pow(2, counter));
+                } else {
+                    throw(msg);
+                }
+            } else {
+                fn();
+            }
+        }
+
+        wait();
+    }
 }
 
-var wait4L=wait4(function () {return typeof L==='undefined'}, "Leaflet is not loaded!");
+var wait4L = wait4(function () {
+    return typeof L === 'undefined'
+}, "Leaflet is not loaded!");
 
